3f0e72ea08b6d5eca6ec3702687c73000a72cdee
AlfonsoAcosta28 <109112791+AlfonsoAcosta28@users.noreply.github.com>
2023-08-22 23:03:35 -0600

    Actualizacion

---
+++ TransportationCompany.Web/TransportationCompany.Client/Utils/JwtTokenValidationSettings.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TransportationCompany.Shared.Config;
+
+namespace TransportationCompany.Client.Utils
+{
+    public interface IJwtTokenValidationSettings
+    {
+        string ValidIssuer { get; }
+        string ValidAudience { get; }
+        string SecretKey { get; }
+        TokenValidationParameters CreateTokenValidationParameters();
+    }
+    public class JwtTokenValidationSettingsFactory : IJwtTokenValidationSettings
+    {
+        private readonly JwtTokenValidationSettings _settings;
+        public string ValidIssuer => _settings.ValidIssuer;
+
+        public string ValidAudience => _settings.ValidAudience;
+
+        public string SecretKey => _settings.SecretKey;
+
+        public TokenValidationParameters CreateTokenValidationParameters()
+        {
+            var result = new TokenValidationParameters
+            {
+                ValidIssuer = ValidIssuer,
+                ValidAudience = ValidAudience,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey)),
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+            return result;
+        }
+
+        public JwtTokenValidationSettingsFactory(IOptions<JwtTokenValidationSettings> options)
+        {
+            _settings = options.Value;
+        }
+    }
+}
